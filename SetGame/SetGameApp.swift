08f5e5c1c6248ede15e176614bb59e112b35c004
Ken Goettler <goettlek@gmail.com>
2021-04-08 17:08:34 -0400

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Ken Goettler on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
